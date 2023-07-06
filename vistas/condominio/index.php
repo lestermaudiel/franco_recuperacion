74724abe251c1125a2876a6cc1d42310fcf0ec78
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-06 10:57:46 -0600

    creacion de Index.php para condominio en vistas

---
+++ vistas/condominio/index.php
@@ -0,0 +1,25 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Ingreso de Condominios</h1>
+        <div class="row justify-content-center">
+            <form action="/franco_recuperacion/controladores/condominio/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                    <label for="condominio_nombre">Condominio</label>
+                        <input type="text" name="condominio_nombre" id="condominio_nombre" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php include_once '../../includes/footer.php'?>

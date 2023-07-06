b6bda4409e15203005d26d5c101271cf47216713
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-06 09:26:04 -0600

    Creacion de navbar

---
+++ includes/navbar.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">COMPLEJO FRANCO</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        CONDOMINIO
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/condominio/index.php">Ingresar Condominio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/condominio/buscar.php">Buscar Condominio</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        VIVIENDA
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/vivienda/index.php">Ingresar vivienda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/vivienda/buscar.php">Buscar vivienda</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        VISITA
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/visita/index.php">Ingresar visita</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/visita/buscar.php">Buscar visita</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        DETALLE DE LAS VISITAS
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/franco_recuperacion/vistas/visita/detalle.php">Ver Detalle</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>

7326272168d96bd271d26e10e1493b849c81759e
lestermaudiel <flestermaudiel2130@gmail.com>
2023-07-06 15:46:23 -0600

    edicion de modificar.php en controladores y vistas.

--- vistas/vivienda/modificar.php
+++ vistas/vivienda/modificar.php
@@ -3,57 +3,59 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require '../../modelos/Vivienda.php';
-    try {
-        $vivienda = new Vivienda($_GET);
 
-        $vivienda = $vivienda->buscar();
-       
-    } catch (PDOException $e) {
-        $error = $e->getMessage();
-    } catch (Exception $e2){
-        $error = $e2->getMessage();
-    }
+try {
+    $vivienda = new Vivienda($_GET);
+    $vivienda = $vivienda->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
 
 require_once '../../modelos/Condominio.php';
-    try {
-        $condominio = new Condominio();
-        $condominio = $condominio->buscar();
 
-    } catch (PDOException $e) {
-        $error = $e->getMessage();
-    } catch (Exception $e2){
-        $error = $e2->getMessage();
-    }
+try {
+    $condominio = new Condominio();
+    $condominio = $condominio->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
 ?>
+
 <?php include_once '../../includes/header.php'?>
-    <div class="container">
-        <h1 class="text-center">Modificar Viviendas de Residentes</h1>
-        <div class="row justify-content-center">
-            <form action="/franco_recuperacion/controladores/vivienda/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
-                <input type="hidden" name="vivienda_id">
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="vivienda_residente">Nombre del Residente</label>
-                        <input type="text" name="vivienda_residente" id="vivienda_residente" class="form-control" required>
-                    </div>
-                </div>
-                <div class="row mb-3">
+<div class="container">
+    <h1 class="text-center">Modificar Viviendas de Residentes</h1>
+    <div class="row justify-content-center">
+        <form action="/franco_recuperacion/controladores/vivienda/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <input type="hidden" name="vivienda_id" value="<?= $vivienda['vivienda_id'] ?>">
+            <div class="row mb-3">
                 <div class="col">
-                        <label for="vivienda_condominio_id">Condominio</label>
-                        <select name="vivienda_condominio_id" id="vivienda_condominio_id" class="form-control">
-                            <option value="">SELECCIONE...</option>
-                            <?php foreach ($condominio as $key => $condominio) : ?>
-                                <option value="<?= $condominio['CONDOMINIO_ID'] ?>"><?= $condominio['CONDOMINIO_NOMBRE'] ?></option>
-                            <?php endforeach?>
-                        </select>
-                    </div>
+                    <label for="vivienda_residente">Nombre del Residente</label>
+                    <input type="text" name="vivienda_residente" id="vivienda_residente" class="form-control" value="<?= $vivienda['vivienda_residente'] ?>" required>
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <button type="submit" class="btn btn-warning w-100">Modificar</button>
-                    </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="vivienda_condominio_id">Condominio</label>
+                    <select name="vivienda_condominio_id" id="vivienda_condominio_id" class="form-control">
+                        <option value="">SELECCIONE...</option>
+                        <?php foreach ($condominio as $cond) : ?>
+                            <option value="<?= $cond['condominio_id'] ?>" <?= ($cond['condominio_id'] == $vivienda['vivienda_condominio_id']) ? 'selected' : '' ?>>
+                                <?= $cond['condominio_nombre'] ?>
+                            </option>
+                        <?php endforeach ?>
+                    </select>
                 </div>
-            </form>
-        </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-warning w-100">Modificar</button>
+                </div>
+            </div>
+        </form>
     </div>
+</div>
 <?php include_once '../../includes/footer.php'?>
